ada91add13209416e16955fbb0f156f0bad84568
Inma Ordóñez <iordonezmarvizon@gmail.com>
2018-09-05 23:13:08 +0200

    Added new feature: upload new ad

--- routes/apiv1/adform.js
+++ routes/apiv1/adform.js
@@ -1,16 +1,74 @@
 'use strict';
 
-//load express router
+//load depencencies
 const express = require('express');
-const router = express.Router();
+const multer = require('multer');
 
 //load Ad module
 const Ad = require('../../models/Ad');
 
-// middleware
+// create router
+const router = express.Router();
+
+// set multer storage settings
+var storage = multer.diskStorage ({
+    destination: 'public/images',
+    filename: function (req, file, callback) {
+      callback(null, file.originalname);
+    }
+  });
+
+const upload = multer({ storage: storage });
+
+// get form view
 router.get('/', async (req, res, next) => {
     try {
-        res.send('ok');
+        res.render('adform', { message: '' });
+    } catch(err) {
+        next(err);
+    }
+});
+
+// POST METHOD TO UPLOAD NEW AD
+router.post('/', upload.single('pic'), async (req, res, next) => {
+    try {
+        //get data
+        const formFields = req.body;
+        // get picture, keep original name and save it in /images/
+        req.file.filename = req.file.originalname;
+        const picture = req.file;
+        // create new object ad
+        const adFields = {
+            name: formFields.name,
+            price: formFields.price,
+            pic: picture.filename,
+        };
+        if(formFields.operation === 'sell') {
+            adFields.selling = true;
+        } else if (formFields.operation === 'buy') {
+            adFields.selling = false;
+        }
+        let tags = [];
+        if(formFields.lifestyle) {
+            tags.push('lifestyle');
+        }
+        if(formFields.motor) {
+            tags.push('motor');
+        }
+        if(formFields.mobile) {
+            tags.push('mobile');
+        }
+        if(formFields.work) {
+            tags.push('work');
+        }
+        if(tags.length > 0) {
+            adFields.tags = tags;
+        }
+        // store new ad in nodepop database
+        const newAd = new Ad(adFields);
+        const savedAd = await newAd.save();
+
+        res.render('adform', { message: 'Your ad has been published!' });
     } catch(err) {
         next(err);
     }
