4b17dc2d2f00dce65b9b99b52bfc429a05c06d72
Inma Ordóñez <iordonezmarvizon@gmail.com>
2018-09-09 12:14:34 +0200

    eslint

--- app.js
+++ app.js
@@ -36,26 +36,27 @@ app.use('/users', require('./routes/users'));
 app.locals.title = '✨Nodepop✨';
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function(_req, _res, next) {
+	next(createError(404));
 });
 
 // error handler
+// eslint-disable-next-line
 app.use(function(err, req, res, next) {
-  // check if validation error
-  if(err.array) {
-    // handle validation error
-    err.status = 422;
-    const errorInfo = err.array({onlyFirstError: true})[0];
-    err.message = `Query not valid: ${errorInfo.param} ${errorInfo.msg}`
-  }
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// check if validation error
+	if(err.array) {
+		// handle validation error
+		err.status = 422;
+		const errorInfo = err.array({onlyFirstError: true})[0];
+		err.message = `Query not valid: ${errorInfo.param} ${errorInfo.msg}`;
+	}
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- install-db.js
+++ install-db.js
@@ -17,54 +17,56 @@ const Ad = require('./models/Ad');
 const User = require('./models/User');
 
 // load ads and users data in database
+
+// eslint-disable-next-line
 conn.once('open', async () => {
-    try {
-        //create interface and ask about deleting db contents
-        const response = await askUser('Do you really want to delete database contents? (if you are not sure, type "no") ');
-        if(response.toLowerCase() !== 'yes') {
-            console.log('Aborted process');
-            process.exit();
-        }
-        //load new content
-        await initAds(ads);
-        await initUsers(users);
+	try {
+		//create interface and ask about deleting db contents
+		const response = await askUser('Do you really want to delete database contents? (if you are not sure, type "no") ');
+		if(response.toLowerCase() !== 'yes') {
+			console.log('Aborted process');
+			process.exit();
+		}
+		//load new content
+		await initAds(ads);
+		await initUsers(users);
 
-        //close connection to database
-        conn.close();
+		//close connection to database
+		conn.close();
 
-    } catch(err) {
-        console.log(err);
-        process.exit(1);
-    }
+	} catch(err) {
+		console.log(err);
+		process.exit(1);
+	}
 });
 
 function askUser(question) {
-    return new Promise((resolve, reject) => {
-        const rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        rl.question(question, function(answer) {
-            rl.close();
-            resolve(answer);
-        });
-    });
-};
+	return new Promise((resolve, reject) => {
+		const rl = readline.createInterface({
+			input: process.stdin,
+			output: process.stdout
+		});
+		rl.question(question, function(answer) {
+			rl.close();
+			resolve(answer);
+		});
+	});
+}
 
 async function initAds(ads) {
-    //delete current documents
-    const deleted = await Ad.deleteMany();
-    console.log(`${deleted.n} ads were deleted`);
-    //load new documents
-    const inserted = await Ad.insertMany(ads);
-    console.log(`${inserted.length} ads were inserted`);
-};
+	//delete current documents
+	const deleted = await Ad.deleteMany();
+	console.log(`${deleted.n} ads were deleted`);
+	//load new documents
+	const inserted = await Ad.insertMany(ads);
+	console.log(`${inserted.length} ads were inserted`);
+}
 
 async function initUsers(users) {
-    //delete current documents
-    const deleted = await User.deleteMany();
-    console.log(`${deleted.n} users were deleted`);
-    //load new documents
-    const inserted = await User.insertMany(users);
-    console.log(`${inserted.length} users were inserted`);
-};
+	//delete current documents
+	const deleted = await User.deleteMany();
+	console.log(`${deleted.n} users were deleted`);
+	//load new documents
+	const inserted = await User.insertMany(users);
+	console.log(`${inserted.length} users were inserted`);
+}
